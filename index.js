61cddaf66f5f0af906b7b137f84059e5cedb6b34
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-11-11 19:09:14 +0530

    done todayy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+const user = require('./MOCK_DATA.json')
+app.get('/api/user',(req,res)=>{
+    return res.json(user)
+})
+app.get('/user',(req,res)=>{
+    const html = `<ul> ${user.map((user)=>
+        `<li> ${user.first_name} </li>`
+    )} </ul>`
+    return res.send(html)
+})
+app.get('/api/user/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    const usr = user.find((userr)=>userr.id===id)
+    res.json(usr)
+})
+app.listen(PORT,()=>{
+    console.log(`server started at port ${PORT}`)
+})
+
+// agar tumhare paas same route hai to sbke lia ye use krkste ho
+// app.route(routename).get().put().patch()
