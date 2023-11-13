d9524ab829bfaf495c05a683f6fcdcb17f3f6232
Devesh  Upadhyay <130208972+deveshxdd@users.noreply.github.com>
2023-11-13 09:30:53 +0530

    done

--- new.js
+++ new.js
@@ -3,11 +3,43 @@ const app = express()
 const users = require('./MOCK_DATA.json')
 const fs= require('fs')
 
-app.use(express.urlencoded({extended: false}))   // ek middleware laga diya data post aane ke lia
+app.use(express.urlencoded({extended: false})) 
+//---> agar ye wala middleware band krde to body undefined aata tha
+
+
+// ek middleware laga diya data post aane ke lia
 
 // jab v koi form data aaega wo usko body me dalne ka kaam krega
+// midleware is function between client and server which have acces of req and res
+
+// creating own middleware
+// sbse phle upar wala middleware chalega fir uska next mtlb ye wala middle ware chalega fir iska next route chalega
+// middleware can make changes to req and res 
+
+// app.use((req , res , next)=>{
+//     console.log("hellow from middleware")
+//     return res.json("hellow from m1")
+// })   -------> agar sirf ye rhega to req yahi atak jaaega aage jaaega hi nhi 
+//     ---------> agar humne middleware me return v laga diya to wo use aage nhi jane degaa
+app.use((req , res , next)=>{
+    console.log("hellow from middleware")
+    // req.muusrname = "devesh"
+    fs.appendFile("log.txt",`/n ${Date.now()}: ${req.method}:  ${req.path}`,(err,data)=>{
+        next()
+    })
+  
+})
+// app.use((req , res , next)=>{
+//     console.log("hellow from middleware 2", req.muusrname)
+//     next()
+// })
+
+
+// iske next pe routes hai to wo call krjaaenge
+
 
 app.get('/api/user',(req,res)=>{
+    // console.log(req.muusrname)
     return res.json(users)
 })
 app.listen(2000,()=>{
